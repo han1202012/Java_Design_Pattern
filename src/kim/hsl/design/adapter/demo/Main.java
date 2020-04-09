9ca994af1bd7ccc31700d2d675d9c32f769f1d11
octopus <octopus_truth@163.com>
2020-04-09 20:53:11 +0800

    update

--- src/kim/hsl/design/adapter/demo/Main.java
+++ src/kim/hsl/design/adapter/demo/Main.java
@@ -1,10 +1,8 @@
 package kim.hsl.design.adapter.demo;
 
 public class Main {
-
     public static void main(String[] args) {
         PhoneCharge phoneCharge = new Adapter();
         phoneCharge.power5V();
     }
-
 }
